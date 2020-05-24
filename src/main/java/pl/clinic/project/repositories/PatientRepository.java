c3b47d0860cb56a5c471b428d60827249e30a278
Jakub <jakub.misiuk@gmail.com>
2020-05-24 09:38:00 +0200

    added repositories, and @Service annotations

---
+++ src/main/java/pl/clinic/project/repositories/PatientRepository.java
@@ -0,0 +1,4 @@
+package pl.clinic.project.repositories;
+
+public class PatientRepository {
+}
--- src/main/java/pl/clinic/project/service/PatientService.java
+++ src/main/java/pl/clinic/project/service/PatientService.java
@@ -1,4 +1,7 @@
 package pl.clinic.project.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PatientService {
 }
