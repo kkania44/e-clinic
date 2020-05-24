079052611f7ab4687bde93515dcfafef49f65d15
kkania44 <kaniakrzysztof99@gmail.com>
2020-05-24 12:00:16 +0200

    userRole fix

--- src/main/java/pl/clinic/project/UserRole.java
+++ src/main/java/pl/clinic/project/UserRole.java
@@ -1,7 +1,16 @@
 package pl.clinic.project;
 
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
 
-    ADMIN, USER_DOCTOR, USER_PATIENT
+    ADMIN("ADMIN"), USER_DOCTOR("USER_DOCTOR"), USER_PATIENT("USER_DOCTOR");
+
+    private UserRole(String role) {
+        this.role = role;
+    }
+
+    private String role;
 
 }
