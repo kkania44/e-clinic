95c7bc0c6ffecd6309443bf97a232451b899406a
Jakub <jakub.misiuk@gmail.com>
2020-05-24 11:02:36 +0200

    added dependencies with PESEL walidator

--- src/main/java/pl/clinic/project/model/Patient.java
+++ src/main/java/pl/clinic/project/model/Patient.java
@@ -1,6 +1,7 @@
 package pl.clinic.project.model;
 
 import lombok.*;
+import org.hibernate.validator.constraints.pl.PESEL;
 import pl.clinic.project.validator.PeselNumberValidator;
 import pl.clinic.project.validator.ValidPeselNumber;
 
@@ -14,6 +15,8 @@ public class Patient {
     private Integer id;
     private String firstName;
     private String lastName;
+
+    @PESEL
     private String peselNumber;
     private String phoneNumber;
 }
