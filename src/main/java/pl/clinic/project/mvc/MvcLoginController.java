bad7b36e3f0cf067342703c3dd8063386d7080f5
kkania44 <kaniakrzysztof99@gmail.com>
2020-06-06 09:28:33 +0200

    no access to login page by logged user

--- src/main/java/pl/clinic/project/mvc/MvcLoginController.java
+++ src/main/java/pl/clinic/project/mvc/MvcLoginController.java
@@ -7,6 +7,8 @@ import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -15,12 +17,14 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
+import pl.clinic.project.UserRole;
 import pl.clinic.project.entities.UserEntity;
 import pl.clinic.project.model.Patient;
 import pl.clinic.project.model.User;
 import pl.clinic.project.service.UserService;
 
 import javax.servlet.http.HttpSession;
+import java.util.Collection;
 
 @Controller
 @RequestMapping("/login")
@@ -38,6 +42,12 @@ public class MvcLoginController {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth instanceof AnonymousAuthenticationToken) {
             return "login.html";
+        }
+        Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+        if (authorities.contains(new SimpleGrantedAuthority("ROLE_"+ UserRole.ADMIN.getName()))) {
+            return "redirect:/users/admin";
+        } else if (authorities.contains(new SimpleGrantedAuthority("ROLE_"+ UserRole.USER_PATIENT.getName()))) {
+            return "redirect:/patients/patientPanel";
         } else {
             return "redirect:/";
         }
