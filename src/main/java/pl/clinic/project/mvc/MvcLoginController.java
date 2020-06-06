6eddb951f026f2180364a8b7c5d842a6de3d5ce2
kkania44 <kaniakrzysztof99@gmail.com>
2020-06-06 09:30:19 +0200

    remove unnecessary imports from login controller

--- src/main/java/pl/clinic/project/mvc/MvcLoginController.java
+++ src/main/java/pl/clinic/project/mvc/MvcLoginController.java
@@ -1,29 +1,14 @@
 package pl.clinic.project.mvc;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.server.Session;
-import org.springframework.context.annotation.Scope;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.ModelAndView;
 import pl.clinic.project.UserRole;
-import pl.clinic.project.entities.UserEntity;
-import pl.clinic.project.model.Patient;
-import pl.clinic.project.model.User;
 import pl.clinic.project.service.UserService;
-
-import javax.servlet.http.HttpSession;
 import java.util.Collection;
 
 @Controller
