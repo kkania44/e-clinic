d4d647b060bca87e942d1d53e5b82ee6ec9c3723
kkania44 <kaniakrzysztof99@gmail.com>
2020-06-20 12:18:49 +0200

    fix redirect from main page

--- src/main/java/pl/clinic/project/mvc/MvcMainPageController.java
+++ src/main/java/pl/clinic/project/mvc/MvcMainPageController.java
@@ -1,5 +1,8 @@
 package pl.clinic.project.mvc;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +13,12 @@ public class MvcMainPageController {
 
     @GetMapping
     String mainPage() {
-        return "mainPage.html";
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth instanceof AnonymousAuthenticationToken) {
+            return "mainPage.html";
+        } else {
+            return "redirect:/login";
+        }
     }
 
 }
