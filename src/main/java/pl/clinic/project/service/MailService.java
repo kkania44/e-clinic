9f7bcb874bfd03ecfb8c5443d222f5e92f7f9c5b
kkania44 <kaniakrzysztof99@gmail.com>
2020-11-13 23:20:50 +0100

    Enable async email sending

--- src/main/java/pl/clinic/project/service/MailService.java
+++ src/main/java/pl/clinic/project/service/MailService.java
@@ -6,6 +6,7 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import pl.clinic.project.model.Email;
 
@@ -16,6 +17,7 @@ public class MailService {
 
     private JavaMailSender mailSender;
 
+    @Async
     public void sendMail(Email email) {
         MimeMessagePreparator mimeMessagePreparator = mimeMessage -> {
             MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
